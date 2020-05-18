dae32eb4a564e977bfdd9cbad8866ca80bc97a7a
lburgueno <lolaburgueno@gmail.com>
2020-05-19 00:17:23 +0200

    engine executes python script - NLP component

---
+++ org.recommender.engine/python_scripts/a_python_script.py
@@ -0,0 +1,6 @@
+import sys
+print ('Number of Arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+print('This is Python Code')
+print('Executing Python')
+print('From Java')
